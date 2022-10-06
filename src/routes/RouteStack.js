aa41434acec15172ef15d13c651cd69b944b7f56
HilariusRyanAB <benedecash@gmail.com>
2022-10-06 09:43:17 +0700

    commit_6

--- src/routes/RouteStack.js
+++ src/routes/RouteStack.js
@@ -9,10 +9,10 @@ const RouteStack = ({data, status, addData, editData}) =>{
   return(
     <Router>
       <Routes>
-        <Route path={'/'} component={Home} element={<Home data={data} addData={addData} editData={editData}/>} />
-        <Route path={'/home'} component={Home} element={<Home data={data} status={status} addData={addData} editData={editData}/>} />
-        <Route path={'/add'} component={Add} element={<Add data={data} addData={addData}/>} />
-        <Route path={'/detail/:id'} component={Detail} element={<Detail data={data} status={status} editData={editData}/>} />
+        <Route path={'/test_ids_ryan/'} component={Home} element={<Home data={data} addData={addData} editData={editData}/>} />
+        <Route path={'/test_ids_ryan/home'} component={Home} element={<Home data={data} status={status} addData={addData} editData={editData}/>} />
+        <Route path={'/test_ids_ryan/add'} component={Add} element={<Add data={data} addData={addData}/>} />
+        <Route path={'/test_ids_ryan/detail/:id'} component={Detail} element={<Detail data={data} status={status} editData={editData}/>} />
       </Routes>
     </Router>
   ); 
